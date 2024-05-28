9fad4294dfdc8b81d2803a16e3097f1e08169622
Roger Alamañac <roger.alamanac@enti.cat>
2024-05-28 22:52:24 +0200

    Si

--- CodiCircuit/CodiCircuit.ino
+++ CodiCircuit/CodiCircuit.ino
@@ -4,12 +4,14 @@
 #define LIGHT_SIGNAL2 A2
 #define LIGHT_SIGNAL3 A3
 #define LIGHT_SIGNAL4 A4
+#define MIN_LIGHT 400
 
 //Ultrasonic sensor
 #define TRIGGER1 11
 #define ECHO1 5
-#define TRIGGER2 10
+#define TRIGGER2 9
 #define ECHO2 3
+#define MIN_TIME_STOPPED 3000
 
 //LEDS D2,D4,D7,D8,D12,D13
 #define RED_LED1 2
@@ -20,7 +22,7 @@
 #define GREEN_LED2 13
 
 // BUZZER
-#define BUZZER 9
+#define BUZZER 6
 
 //Configuration parameters
 //Traffic light
@@ -29,24 +31,24 @@
 #define TIME_IN_RED 9000
 #define CORRECT_DISTANCE 50
 //Punctuation
-#define MAX_POINTS 100
-#define MINUS_TRAFFIC_LIGHT 20
-#define MINUS_VELOCITY 10
-#define MINUS_TRAFFIC_SIGNALS 25
-#define MINUS_COLLISION 15
+#define MAX_POINTS 9
+#define MINUS_TRAFFIC_LIGHT 2
+#define MINUS_VELOCITY 1
+#define MINUS_TRAFFIC_SIGNALS 2
+#define MINUS_COLLISION 2
 
 int punctuation = MAX_POINTS;
-unsigned long referenceTime;
+unsigned long referenceTime, timeStopped;
 long t_pulse1, t_pulse2, distance_cm1, distance_cm2, velocity;
-bool isRed, isYellow, isGreen;
+bool isRed, isYellow, isGreen, hasTimed;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LIGHT_SIGNAL0, OUTPUT);
-  pinMode(LIGHT_SIGNAL1, OUTPUT);
-  pinMode(LIGHT_SIGNAL2, OUTPUT);
-  pinMode(LIGHT_SIGNAL3, OUTPUT);
-  pinMode(LIGHT_SIGNAL4, OUTPUT);
+  pinMode(LIGHT_SIGNAL0, INPUT);
+  pinMode(LIGHT_SIGNAL1, INPUT);
+  pinMode(LIGHT_SIGNAL2, INPUT);
+  pinMode(LIGHT_SIGNAL3, INPUT);
+  pinMode(LIGHT_SIGNAL4, INPUT);
   pinMode(TRIGGER1, OUTPUT);
   pinMode(ECHO1, INPUT);
   pinMode(TRIGGER2, OUTPUT);
@@ -70,6 +72,8 @@ void setup() {
   isRed = true;
   isYellow = false;
   isGreen = false;
+  timeStopped = 0;
+  hasTimed = false;
 }
 
 void loop() {
@@ -81,12 +85,32 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIGGER1, LOW);
   digitalWrite(TRIGGER2, LOW);
-  
-  t_pulse1 = pulseIn(ECHO1, HIGH);
-  t_pulse2 = pulseIn(ECHO2, HIGH);
+
+  t_pulse1 = pulseIn(ECHO1, HIGH, 30000);
+  t_pulse2 = pulseIn(ECHO2, HIGH, 30000);
   distance_cm1 = t_pulse1 * 10 / 292 / 2;
   distance_cm2 = t_pulse2 * 10 / 292 / 2;
+  /*Serial.print("Distance 1: ");
   Serial.println(distance_cm1);
+  Serial.print("Distance 2: ");
+  Serial.println(distance_cm2);*/
+
+  int ldr0 = analogRead(LIGHT_SIGNAL0);
+  /*int ldr1 = analogRead(LIGHT_SIGNAL1);
+  int ldr2 = analogRead(LIGHT_SIGNAL2);
+  int ldr3 = analogRead(LIGHT_SIGNAL3);
+  int ldr4 = analogRead(LIGHT_SIGNAL4);*/
+
+  Serial.print("LDR 0: ");
+  Serial.println(ldr0);
+  /*Serial.print("LDR 1: ");
+  Serial.println(ldr1);
+  Serial.print("LDR 2: ");
+  Serial.println(ldr2);
+  Serial.print("LDR 3: ");
+  Serial.println(ldr3);
+  Serial.print("LDR 4: ");
+  Serial.println(ldr4);*/
   // SEMÀFORS
   if (isRed && millis() - referenceTime > TIME_IN_RED) {  // RED -> GREEN
     isRed = false;
@@ -118,15 +142,33 @@ void loop() {
 
   // SENSORS DE ULTRASÓ
 
-  if(distance_cm1 < 16 && isRed){
+  if (distance_cm1 < 16 && isRed) {
+    tone(BUZZER, 500, 4000);
+  }
+  /*
+  if (distance_cm2 < 16 && isRed) {
     tone(BUZZER, 100);
-  }
-  else{
+  } else {
     noTone(BUZZER);
-  }
-  /*if(distance_cm2 < 16 && isRed){
-    tone(BUZZER, 800, 64);
   }*/
+
+  if (ldr0 < MIN_LIGHT) {
+    // Si la luz es menor que el umbral y no se ha registrado el tiempo de parada
+    if (!hasTimed) {
+      hasTimed = true;
+      timeStopped = millis();
+    } else if (millis() - timeStopped > MIN_TIME_STOPPED + 3000) {
+      // Si la luz es menor que el umbral y ha pasado el tiempo mínimo más un margen de 3 segundos
+      hasTimed = false;
+      timeStopped = 0;
+    }
+  } else {
+    // Si la luz es mayor que el umbral antes de que el tiempo mínimo pase
+    if (hasTimed && millis() - timeStopped < MIN_TIME_STOPPED) {
+      tone(BUZZER, 900, 4000); // Suena el buzzer
+    }
+    hasTimed = false;
+    timeStopped = 0;
+  }
+
 }
-
-
--- CodiCotxe/CodiCotxe/CodiCotxe.ino
+++ CodiCotxe/CodiCotxe/CodiCotxe.ino
@@ -1,97 +1,121 @@
+#include <LiquidCrystal.h>
 //#include <SoftawareSerial.h>
 //LED
 #define PIN_LED 2
+//PANTALLA
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 // Motor: direction of rotation
 #define FORWARD 0
 #define BACKWARD 1
 // Motor driver interface
 #define MOTOR_DRIVER_IN_1 6
 #define MOTOR_DRIVER_IN_2 9
-#define MOTOR_DRIVER_IN_3 11
-#define MOTOR_DRIVER_IN_4 10
-#define MOTOR_DRIVER_ENABLE1 5
-#define MOTOR_DRIVER_ENABLE2 8
+#define MOTOR_DRIVER_IN_3 10
+#define MOTOR_DRIVER_IN_4 7
+#define MOTOR_DRIVER_ENABLE1 8
+#define MOTOR_DRIVER_ENABLE2 13
 //Bluetooth
 #define PIN_RX 0
 #define PIN_TX 1
 //SoftwareSerial blue(PIN_TX, PIN_RX);
 //Variables
 char letra;
+float motorVelocity;
 bool motor_rotation;
+int targetVelocity = 120;
 void setup() {
- pinMode(MOTOR_DRIVER_IN_1, OUTPUT);
- pinMode(MOTOR_DRIVER_IN_2, OUTPUT);
- pinMode(MOTOR_DRIVER_IN_3, OUTPUT);
- pinMode(MOTOR_DRIVER_IN_4, OUTPUT);
- pinMode(MOTOR_DRIVER_ENABLE1, OUTPUT);
- pinMode(MOTOR_DRIVER_ENABLE2, OUTPUT);
- pinMode(PIN_RX, INPUT);
- pinMode(PIN_TX, OUTPUT);
+  pinMode(MOTOR_DRIVER_IN_1, OUTPUT);
+  pinMode(MOTOR_DRIVER_IN_2, OUTPUT);
+  pinMode(MOTOR_DRIVER_IN_3, OUTPUT);
+  pinMode(MOTOR_DRIVER_IN_4, OUTPUT);
+  pinMode(MOTOR_DRIVER_ENABLE1, OUTPUT);
+  pinMode(MOTOR_DRIVER_ENABLE2, OUTPUT);
+  pinMode(PIN_RX, INPUT);
+  pinMode(PIN_TX, OUTPUT);
 
- //blue.begin(9600);
- Serial.begin(9600);
+  //blue.begin(9600);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
 
- //Inicialització
- digitalWrite (MOTOR_DRIVER_IN_1, LOW);
- digitalWrite (MOTOR_DRIVER_IN_2, LOW);
- digitalWrite (MOTOR_DRIVER_IN_3, LOW);
- digitalWrite (MOTOR_DRIVER_IN_4, LOW);
- analogWrite (MOTOR_DRIVER_ENABLE1, 0);
- analogWrite (MOTOR_DRIVER_ENABLE2, 0);
+  //Inicialització
+  digitalWrite(MOTOR_DRIVER_IN_1, LOW);
+  digitalWrite(MOTOR_DRIVER_IN_2, LOW);
+  digitalWrite(MOTOR_DRIVER_IN_3, LOW);
+  digitalWrite(MOTOR_DRIVER_IN_4, LOW);
+  analogWrite(MOTOR_DRIVER_ENABLE1, 0);
+  analogWrite(MOTOR_DRIVER_ENABLE2, 0);
 
 
- motor_rotation = FORWARD;
+  motor_rotation = FORWARD;
+  motorVelocity = 120;
+  letra = 'z';
 }
 
 void loop() {
-    if(Serial.available()>0){
-      letra = Serial.read();
-    }
-    switch (letra) {
-    case 'w': // Avanzar
-      Serial.println("Avanza");
-      analogWrite(MOTOR_DRIVER_ENABLE1, 255); // Velocidad máxima
-      analogWrite(MOTOR_DRIVER_ENABLE2, 255);
+  lcd.setCursor(0, 0);
+  lcd.print(motorVelocity - 120);
+  if (Serial.available() > 0) {
+    letra = Serial.read();
+  }
+  switch (letra) {
+    case 'w':  // Avanzar
+      if (motorVelocity < 255) {
+        motorVelocity += 10;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("Avanza");
+      analogWrite(MOTOR_DRIVER_ENABLE1, (motorVelocity / 3));  // Velocidad máxima
+      analogWrite(MOTOR_DRIVER_ENABLE2, (motorVelocity / 3));
       digitalWrite(MOTOR_DRIVER_IN_1, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_2, LOW);
       digitalWrite(MOTOR_DRIVER_IN_3, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_4, LOW);
       break;
-    case 's': // Retroceder
-      Serial.println("Retrocede");
-      analogWrite(MOTOR_DRIVER_ENABLE1, 255); // Velocidad máxima
-      analogWrite(MOTOR_DRIVER_ENABLE2, 255);
+    case 's':  // Retroceder
+      if (motorVelocity > 14) {
+        motorVelocity -= 15;
+      } else {
+        motorVelocity = 0;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("Retrocede");
+      analogWrite(MOTOR_DRIVER_ENABLE1, (motorVelocity / 3));  // Velocidad máxima
+      analogWrite(MOTOR_DRIVER_ENABLE2, (motorVelocity / 3));
       digitalWrite(MOTOR_DRIVER_IN_1, LOW);
       digitalWrite(MOTOR_DRIVER_IN_2, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_3, LOW);
       digitalWrite(MOTOR_DRIVER_IN_4, HIGH);
       break;
-    case 'a': // Girar a la izquierda
-      Serial.println("Izquierda");
-      analogWrite(MOTOR_DRIVER_ENABLE1, 255); // Velocidad máxima
-      analogWrite(MOTOR_DRIVER_ENABLE2, 255);
+    case 'a':  // Girar a la izquierda
+      if (motorVelocity < 255) {
+        motorVelocity += 10;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("Izquierda");
+      analogWrite(MOTOR_DRIVER_ENABLE1, (motorVelocity / 3));  // Velocidad máxima
+      analogWrite(MOTOR_DRIVER_ENABLE2, (motorVelocity / 3));
       digitalWrite(MOTOR_DRIVER_IN_1, LOW);
       digitalWrite(MOTOR_DRIVER_IN_2, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_3, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_4, LOW);
       break;
-    case 'd': // Girar a la derecha
-      Serial.println("Derecha");
-      analogWrite(MOTOR_DRIVER_ENABLE1, 255); // Velocidad máxima
-      analogWrite(MOTOR_DRIVER_ENABLE2, 255);
+    case 'd':  // Girar a la derecha
+      if (motorVelocity < 255) {
+        motorVelocity += 10;
+      }
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("Derecha");
+      analogWrite(MOTOR_DRIVER_ENABLE1, (motorVelocity / 3));  // Velocidad máxima
+      analogWrite(MOTOR_DRIVER_ENABLE2, (motorVelocity / 3));
       digitalWrite(MOTOR_DRIVER_IN_1, HIGH);
       digitalWrite(MOTOR_DRIVER_IN_2, LOW);
       digitalWrite(MOTOR_DRIVER_IN_3, LOW);
       digitalWrite(MOTOR_DRIVER_IN_4, HIGH);
-      break;
-    default:
-      // Detener los motores si se recibe una letra no reconocida
-      digitalWrite(MOTOR_DRIVER_IN_1, LOW);
-      digitalWrite(MOTOR_DRIVER_IN_2, LOW);
-      digitalWrite(MOTOR_DRIVER_IN_3, LOW);
-      digitalWrite(MOTOR_DRIVER_IN_4, LOW);
-      analogWrite(MOTOR_DRIVER_ENABLE1, 0);
-      analogWrite(MOTOR_DRIVER_ENABLE2, 0);
       break;
   }
+  letra = 'z';
 }
